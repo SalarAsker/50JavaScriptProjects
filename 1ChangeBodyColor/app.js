71e3f284928a2ca8ed01da58420940b18856718b
Salar Asker Zada <salar.askerzada@uddevalla.se>
2022-09-28 16:44:16 +0200

    Clear

---
+++ 1ChangeBodyColor/app.js
@@ -0,0 +1,10 @@
+// 1 load
+// addEventListener('load', function(){
+//     this.document.body.style.backgroundColor = 'red';
+// })
+
+// 2 DOMContentLoaded
+
+addEventListener('DOMContentLoaded', function(){
+    this.document.body.style.backgroundColor = 'blue';
+})
